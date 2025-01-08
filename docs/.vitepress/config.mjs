371fea32cb38de03e2bef56baad8b0133843df3f
wangyz_P06 <wangyz@strongdata.com.cn>
2025-01-08 16:30:55 +0800

    修改base路径

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/blog/',
   title: "个人博客",
   description: "小屁正的个人博客,基于vitepress实现",
   lang: 'en-US',
